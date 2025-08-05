c69bc5b02d611fe036296c0fb30d6a8103f17d15
ajwhalleydata <ajwhalleydata@gmail.com>
2025-08-05 14:34:28 -0400

    config update

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), sitemap(), tailwind()]
+  integrations: [mdx(), sitemap(), tailwind()],
+  base: '/'
 });
